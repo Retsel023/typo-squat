4678164a7809a94bcbf9006d6cf04e33158de947
Lester <lester.weijers@gmail.com>
2024-12-12 09:59:07 +0100

    Update install.sh

--- ail-typo-squatting V-2/install.sh
+++ ail-typo-squatting V-2/install.sh
@@ -95,7 +95,13 @@ printf '%s\n' '#!/bin/bash' \
 
 # Make the generated script executable
 sudo chmod +x "$SCRIPT_PATH"
-
+touch /home/ail-typo-squatting/resolvers.txt
+printf '%s\n' '1.1.1.1' \
+'1.0.0.1' \
+'9.9.9.9' \
+'8.8.8.8' \
+'208.67.222.222' \
+| sudo tee /home/ail-typo-squatting/resolvers.txt > /dev/null
 
 # Use printf to write the Python script content to the target file
 printf '%s\n' 'import openpyxl' \
@@ -208,10 +214,12 @@ printf '%s\n' 'import openpyxl' \
 
 git clone https://github.com/typosquatter/ail-typo-squatting.git /home/ail-typo-squatting/ail-typo-squatting
 pip install -r /home/ail-typo-squatting/ail-typo-squatting/requirements.txt --break-system-packages
-pip install openpyxl retrie --break-system-packages
+pip install openpyxl retrie requests tldextract --break-system-packages
 
 touch /home/ail-typo-squatting/usage.txt
 printf '%s\n' './automate.sh <domainlist> <company name>' | sudo tee /home/ail-typo-squatting/usage.txt
 
-
+#if urlib3 keeps failing
+#apt remove python3-urllib3
+#pip install urllib3 --break-system-packages
 
