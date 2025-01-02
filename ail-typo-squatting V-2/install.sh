3b2680aca5239986311bcb04d353f9b69ae492c1
Lester <lester.weijers@gmail.com>
2025-01-02 16:14:45 +0100

    Update install.sh

--- ail-typo-squatting V-2/install.sh
+++ ail-typo-squatting V-2/install.sh
@@ -1,5 +1,5 @@
 apt update && apt upgrade -y
-apt install -y python3 python3-pip gowitness git subfinder massdns
+apt install -y python3 python3-pip gowitness git subfinder massdns cron
 
 mkdir /home/ail-typo-squatting
 mkdir /home/ail-typo-squatting/output
