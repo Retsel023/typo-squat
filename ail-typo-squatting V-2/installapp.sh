78ad85a3a2fd9abb0a7a8180827b293d42c8f23b
Lester <lester.weijers@gmail.com>
2025-01-08 11:35:36 +0100

    Update installapp.sh

--- ail-typo-squatting V-2/installapp.sh
+++ ail-typo-squatting V-2/installapp.sh
@@ -1,2 +1,13 @@
 pip install flask werkzeug
 
+APP="/home/ail-typo-squatting/app.py"
+TEMPLATES="/home/ail-typo-squatting/templates"
+INDEX="/home/ail-typo-squatting/templates/index.html"
+UPLOAD="/home/ail-typo-squatting/templates/upload_domain_list.html"
+COMPANY="/home/ail-typo-squatting/templates/company_files.html"
+
+touch "$APP"
+mkdir "$TEMPLATES"
+touch "$INDEX"
+touch "$UPLOAD"
+touch "$COMPANY"
